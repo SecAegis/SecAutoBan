5e785357d0e44db1684f6a6a03d2875156e86a89
SecAegis <130388298+SecAegis@users.noreply.github.com>
2025-06-16 13:26:06 +0800

    add lib

--- device/sdk/python/setup.py
+++ device/sdk/python/setup.py
@@ -13,5 +13,5 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/SecAegis/SecAutoBan",
     packages=setuptools.find_packages(),
-    install_requires = ["pycryptodome", "websocket-client"]
+    install_requires = ["pycryptodome", "websocket-client", "ipaddress"]
 )
