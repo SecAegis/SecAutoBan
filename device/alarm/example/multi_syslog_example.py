95bb398282bbd27d3907aafa52357625bb7921ce
SecAegis <130388298+SecAegis@users.noreply.github.com>
2025-06-22 22:00:53 +0800

    修改至线程安全

--- device/alarm/example/multi_syslog_example.py
+++ device/alarm/example/multi_syslog_example.py
@@ -1,6 +1,6 @@
 import socket
 import socketserver
-from threading import Timer
+from threading import Lock, Timer
 
 
 def reset_count():
@@ -10,16 +10,18 @@ def reset_count():
         for i in message_count:
             message += " [" + i + "]" + str(message_count[i]) + "条"
         print(message)
-        message_count = {}
+        with message_count_lock:
+            message_count = {}
     Timer(3600, reset_count).start()
 
 
 def add_count(device_name):
     global message_count
-    if device_name not in message_count:
-        message_count[device_name] = 1
-    else:
-        message_count[device_name] += 1
+    with message_count_lock:
+        if device_name not in message_count:
+            message_count[device_name] = 1
+        else:
+            message_count[device_name] += 1
 
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
     def handle(self):
@@ -27,16 +29,17 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
         if len(data) < 7:
             return
         if data[7] == "xxx1":
-            add_count("xxx1")
             socket.socket(socket.AF_INET, socket.SOCK_DGRAM).sendto(b''.join(data[7:]), ("127.0.0.1", 515))
+            add_count("xxx1")
         elif data[7] == "xxx2":
-            add_count("xxx2")
             socket.socket(socket.AF_INET, socket.SOCK_DGRAM).sendto(b''.join(data[7:]), ("127.0.0.1", 516))
+            add_count("xxx2")
         else:
             pass
 
 if __name__ == "__main__":
     message_count = {}
+    message_count_lock = Lock()
     listen_syslog_udp_port = 514
     with socketserver.ThreadingUDPServer(("0.0.0.0", listen_syslog_udp_port), lambda *args: SyslogUDPHandler(*args)) as server:
         print("[+] 监听SysLog端口: " + str(listen_syslog_udp_port) + "/UDP")
